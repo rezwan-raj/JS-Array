21822deb93f29ccf4126faca4b95757f8d45a1be
rezwan-raj <rezwanahmedcse@gmail.com>
2026-09-05 21:51:27 +0600

    sorting of js

---
+++ sort.js
@@ -0,0 +1,13 @@
+const person = ["John", "Jane", "Alice", "Bob"];
+const sortedPerson = person.sort();
+console.log(sortedPerson);
+
+const numbers = [5, 2, 9, 1, 5, 6];
+
+//assending -->smaller to larger
+//desending --> larger to smaller
+
+const numbersAsc = numbers.sort();//ei sort majhe moddhe kaj kore na
+
+
+console.log(numbersAsc);
